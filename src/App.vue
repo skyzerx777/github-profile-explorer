c83dfbaba804e67c451e072d97953e3253d9e4a0
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2026-06-12 13:56:32 +0300

    Info rendering draft

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import SearchBar from './components/SearchBar.vue';
+import UserProfileInfo from './components/UserProfileInfo.vue';
 import type { IUser } from './types.ts';
 import '/src/App.css';
 
@@ -14,4 +15,5 @@ const loading = ref(false);
 		@update-loading="loading = $event"
 		:loading
 	/>
+	<UserProfileInfo :selectedUser :loading />
 </template>
---
+++ src/components/UserProfileInfo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { IUser } from '@/types';
+
+defineProps<{ selectedUser: IUser | null; loading: boolean }>();
+</script>
+
+<template>
+	<p v-if="loading">Loading...</p>
+	<div v-if="selectedUser && !loading">
+		<p v-for="(value, key) in selectedUser">{{ key }}: {{ value }}</p>
+	</div>
+</template>
